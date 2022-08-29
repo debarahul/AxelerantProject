d3a237522cc7dd9736ea4952195f6699fb715692
Debasish Nayak <debasishn414@gmail.com>
2022-08-30 00:17:50 +0530

    Testcases add for sorting functionality

--- pageObjects/DressFlow.py
+++ pageObjects/DressFlow.py
@@ -1,10 +1,14 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import Select
 
 class DressFlow:
     link_woman_text = "//a[@title='Women']"
     link_sumdress_text = "Summer Dresses"
     link_evndress_text = "Evening Dresses"
+    link_pricelist_xpath = "//span[@class='price product-price']"
+    button_dropdown_id = "selectProductSort"
+    dropdown_text = "Price: Highest first"
 
     def __init__(self,driver):
         self.driver = driver
@@ -23,5 +27,19 @@ class DressFlow:
         act.move_to_element(woman).perform()
         dress = self.driver.find_element(By.LINK_TEXT, self.link_evndress_text)
         act.move_to_element(dress).click().perform()
+
+    def getPriceList(self):
+        listing = self.driver.find_elements(By.XPATH, self.link_pricelist_xpath)
+        pricelist = []
+        for i in listing:
+            j = i.text
+            if j != '':
+                pricelist.append(float(j.replace("$", "")))
+        return pricelist
+
+    def setSorting(self):
+        scsort = Select(self.driver.find_element(By.ID, self.button_dropdown_id))
+        scsort.select_by_visible_text(self.dropdown_text)
+
 
     
--- testCases/test_dress_flow_validate.py
+++ testCases/test_dress_flow_validate.py
@@ -31,4 +31,37 @@ class Test_dress_flow:
             assert False
 
 
+    @pytest.mark.regression
+    @pytest.mark.sanity
+    def test_sorting(self,setup):
+        time.sleep(3)
+        self.logger.info("test_sorting")
+        self.driver = setup
+        self.driver.implicitly_wait(0.5)
+        self.driver.get(self.baseURL)
+        self.driver.get(self.baseURL)
+        self.df = DressFlow(self.driver)
+        self.df.selectSumDress()
+        self.logger.info("Successfully Landed On Summer Dress Listing Page")
+
+        self.logger.info("Verify The Sorting")
+        beforelist = self.df.getPriceList()
+        self.df.setSorting()
+        time.sleep(8)
+        afterlist = self.df.getPriceList()
+        beforelist.sort(reverse=True)
+
+        self.logger.info("Compare Before And After Sorting Result")
+        if beforelist == afterlist:
+            assert True
+            self.driver.close()
+            self.logger.info("Sorting is successful, Testcase Passed")
+        else:
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_sorting.png")
+            self.driver.close()
+            self.logger.error("Sorting doesn't work, Testcase Failed")
+            assert False
+
+
+
 
