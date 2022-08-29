1cdd1458a153e9694c1e462a244257e854c90e5d
Debasish Nayak <debasishn414@gmail.com>
2022-08-30 01:42:47 +0530

    Added testcases for checkbox functionality

--- pageObjects/DressFlow.py
+++ pageObjects/DressFlow.py
@@ -9,6 +9,9 @@ class DressFlow:
     link_pricelist_xpath = "//span[@class='price product-price']"
     button_dropdown_id = "selectProductSort"
     dropdown_text = "Price: Highest first"
+    checkbox_instock_xpath = "//input[@name='layered_quantity_1']"
+    value_instock_xpath = "//span[@class='available-now']"
+    alert_noproduct_xpath = "//p[contains(@class,'warning')]"
 
     def __init__(self,driver):
         self.driver = driver
@@ -41,5 +44,17 @@ class DressFlow:
         scsort = Select(self.driver.find_element(By.ID, self.button_dropdown_id))
         scsort.select_by_visible_text(self.dropdown_text)
 
+    def clickCheckBox(self):
+        self.driver.find_element(By.XPATH, self.checkbox_instock_xpath).click()
 
-    
+    def getInstockList(self):
+        lists = self.driver.find_elements(By.XPATH, self.value_instock_xpath)
+        instocklist = []
+        for i in lists:
+            j = i.text
+            instocklist.append(j)
+        return instocklist
+
+    def getAlertMsg(self):
+        message = self.driver.find_element(By.XPATH,  self.alert_noproduct_xpath).text
+        return message
--- testCases/test_dress_flow_validate.py
+++ testCases/test_dress_flow_validate.py
@@ -33,9 +33,8 @@ class Test_dress_flow:
 
     @pytest.mark.regression
     @pytest.mark.sanity
-    def test_sorting(self,setup):
-        time.sleep(3)
-        self.logger.info("test_sorting")
+    def test_sorting_result(self,setup):
+        self.logger.info("test_sorting_result")
         self.driver = setup
         self.driver.implicitly_wait(0.5)
         self.driver.get(self.baseURL)
@@ -44,7 +43,7 @@ class Test_dress_flow:
         self.df.selectSumDress()
         self.logger.info("Successfully Landed On Summer Dress Listing Page")
 
-        self.logger.info("Verify The Sorting")
+        self.logger.info("Verify The Sorting Result")
         beforelist = self.df.getPriceList()
         self.df.setSorting()
         time.sleep(8)
@@ -61,6 +60,44 @@ class Test_dress_flow:
             self.driver.close()
             self.logger.error("Sorting doesn't work, Testcase Failed")
             assert False
+
+
+    @pytest.mark.regression
+    @pytest.mark.sanity
+    def test_checkbox_result(self,setup):
+        self.logger.info("test_checkbox_result")
+        self.driver = setup
+        self.driver.implicitly_wait(0.5)
+        self.driver.get(self.baseURL)
+        self.driver.get(self.baseURL)
+        self.df = DressFlow(self.driver)
+        self.df.selectSumDress()
+        self.logger.info("Successfully Landed On Summer Dress Listing Page")
+
+        self.logger.info("Verify The Checkbox Result")
+        beforecheck = self.df.getInstockList()
+        self.logger.info("The Check Box Test Is Based On Value {}".format(beforecheck[1]))
+
+        self.df.clickCheckBox()
+        time.sleep(5)
+        msg = self.df.getAlertMsg()
+        if msg == "There are no products.":
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_checkbox_result.png")
+            self.driver.close()
+            self.logger.error("After Checkbox Select There Are No Product, Testcase Failed")
+            assert False
+        else:
+            aftercheck = self.df.getInstockList()
+            if beforecheck == aftercheck:
+                assert True
+                self.driver.close()
+                self.logger.info("After Checkbox There Are Expected Result Coming, Testcase Passed")
+            else:
+                self.driver.save_screenshot(".\\Screenshots\\" + "test_checkbox_result.png")
+                self.driver.close()
+                self.logger.error("After Checkbox Select There Are Different List, Testcase Failed")
+                assert False
+
 
 
 
